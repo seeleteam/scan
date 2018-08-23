8ae2f1b53e1d41838de61d28fd1c42e1c95cf5b9
MiyaFlower <yangmingyan1987@163.com>
2018-08-23 12:12:44 +0800

    adjustment http request interception

--- src/untils/request.js
+++ src/untils/request.js
@@ -4,45 +4,7 @@ import jsonp from 'jsonp'
 import lodash from 'lodash'
 import pathToRegexp from 'path-to-regexp'
 import { YQL, CORS } from './config'
-import { Message, Loading, MessageBox } from 'element-ui'
-
-// timeout
-axios.defaults.timeout = 5000
-// http request interceptor
-var loadinginstace
-axios.interceptors.request.use(config => {
-  // element ui Loading
-  loadinginstace = Loading.service({
-    lock: true,
-    text: '',
-    spinner: 'loadingIcon',
-    background: 'rgba(255, 255, 255, 0.7)'
-  })
-  return config
-}, error => {
-  MessageBox({
-    title: 'Tips',
-    message: 'Load timeout',
-    callback: action => {
-      loadinginstace.close()
-    }
-  })
-  return Promise.reject(error)
-})
-// http Response interceptor
-axios.interceptors.response.use(data => {
-  loadinginstace.close()
-  return data
-}, error => {
-  MessageBox({
-    title: 'Tips',
-    message: 'request was aborted',
-    callback: action => {
-      loadinginstace.close()
-    }
-  })
-  return Promise.reject(error)
-})
+import { Message } from 'element-ui'
 
 const fetch = (options) => {
   let {
