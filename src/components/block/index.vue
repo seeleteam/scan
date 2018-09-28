8222ceaa76602530de73f48314209a34389a155b
MiyaFlower <yangmingyan1987@163.com>
2018-09-28 16:14:49 +0800

    modify block

--- src/components/block/index.vue
+++ src/components/block/index.vue
@@ -1,101 +1,69 @@
 <template>
-    <div class="page-wrap">
-        <div class="wrap lg-show">
-          <Header></Header>
-        </div>
-        <div class="sm-show page-sm-header-wrap">
-          <smHeader></smHeader>
-        </div>
-        <div class="sm-show sm-search-input-wrap">
-          <searchInput></searchInput>
-        </div>
-        <div class="main-wrap">
-          <div class="wrap">
-            <div class="describe-title-wrap">
-              <span class="title">{{$t('navs.block')}}</span>
-              <ul class="link-wrap">
-                <li><a href="/">{{$t("navs.home")}}</a></li>
-                <li><i class="el-icon-arrow-right"></i></li>
-                <li class="current">{{$t('navs.block')}}</li>
-              </ul>
-            </div>
-            <ShardSelect></ShardSelect>
-            <el-table
-              class="list-wrap"
-              :data="blocksList"
-              :empty-text="$t('message.noData')"
-              style="width: 100%">
-              <el-table-column
-                prop="height"
-                :label="$t('listHeader.height')"
-                min-width="120">
-                <template slot-scope="scope" class="">
-                  <router-link :to="{path: '/block/detail', query: { height: scope.row.height, s: shardValue }}">
-                  <span class="table-link-color height">{{scope.row.height}}</span>
-                  </router-link>
-                </template>
-              </el-table-column>
-              <el-table-column
-                prop="age"
-                :label="$t('listHeader.age')"
-                min-width="120">
-                <template slot-scope="scope">
-                  <span class="age">{{scope.row.age}}</span>
-                </template>
-              </el-table-column>
-              <el-table-column
-                prop="txn"
-                min-width="100"
-                :label="$t('listHeader.txn')">
-                <template slot-scope="scope">
-                  <span>{{scope.row.txn}}</span>
-                </template>
-              </el-table-column>
-              <!-- <el-table-column
-                prop="address"
-                :label="$t('listHeader.uncles')">
-              </el-table-column> -->
-              <el-table-column
-                prop="miner"
-                min-width="280"
-                :label="$t('listHeader.miner')">
-                <template slot-scope="scope">
-                  <router-link :to="{path: '/account/detail', query: { address: scope.row.miner }}">
-                    <span class="list-content miner table-link-color">{{scope.row.miner}}</span>
-                  </router-link>
-                </template>
-              </el-table-column>
-              <el-table-column
-                prop="reward"
-                :label="$t('listHeader.reward')">
-              </el-table-column>
-              <el-table-column
-                prop="fee"
-                :label="$t('listHeader.fee')">
-                <template slot-scope="scope">{{scope.row.fee}} <span class="unit">Fan</span></template>
-              </el-table-column>
-              <!-- <el-table-column
-                prop="address"
-                :label="$t('listHeader.difficulty')">
-              </el-table-column>
-              <el-table-column
-                prop="address"
-                :label="$t('listHeader.hashRate')">
-              </el-table-column> -->
-            </el-table>
-            <el-pagination
-              class="el-pagination-wrap fr"
-              @size-change="handleSizeChange"
-              @current-change="handleCurrentChange"
-              :current-page="page"
-              :page-size="pageSize"
-              layout="prev, pager, next"
-              :total="total">
-            </el-pagination>
-          </div>
-        </div>
-        <Footer></Footer>
+  <div class="page-wrap">
+    <Header></Header>
+    <div class="sm-show page-sm-header-wrap">
+      <smHeader></smHeader>
     </div>
+    <div class="sm-show sm-search-input-wrap">
+      <searchInput></searchInput>
+    </div>
+    <div class="main-wrap">
+      <div class="wrap">
+        <div class="describe-title-wrap">
+          <vue-particles color="#fff" :particleOpacity="0.7" :particlesNumber="80" shapeType="circle" :particleSize="4" linesColor="#000" :linesWidth="1" :lineLinked="true" :lineOpacity="0.4" :linesDistance="150" :moveSpeed="3" :hoverEffect="true" hoverMode="grab" :clickEffect="true" clickMode="push" class="particles"></vue-particles>
+          <span class="title">{{$t('navs.block')}}</span>
+          <ul class="link-wrap">
+            <li>
+              <a href="/">{{$t("navs.home")}}</a>
+            </li>
+            <li>
+              <i class="el-icon-arrow-right"></i>
+            </li>
+            <li class="current">{{$t('navs.block')}}</li>
+          </ul>
+        </div>
+        <ShardSelect></ShardSelect>
+        <el-table class="list-wrap" :data="blocksList" :empty-text="$t('message.noData')" style="width: 100%">
+          <el-table-column prop="height" :label="$t('listHeader.height')" min-width="120">
+            <template slot-scope="scope" class="">
+              <router-link :to="{path: '/block/detail', query: { height: scope.row.height, s: shardValue }}">
+                <span class="table-link-color height">{{scope.row.height}}</span>
+              </router-link>
+            </template>
+          </el-table-column>
+          <el-table-column prop="age" :label="$t('listHeader.age')" min-width="120">
+            <template slot-scope="scope">
+              <span class="age">{{scope.row.age}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="txn" min-width="100" :label="$t('listHeader.txn')">
+            <template slot-scope="scope">
+              <span>{{scope.row.txn}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="miner" min-width="280" :label="$t('listHeader.miner')">
+            <template slot-scope="scope">
+              <router-link :to="{path: '/account/detail', query: { address: scope.row.miner }}">
+                <span class="list-content miner table-link-color">{{scope.row.miner}}</span>
+              </router-link>
+            </template>
+          </el-table-column>
+          <el-table-column prop="reward" :label="$t('listHeader.reward')">
+          </el-table-column>
+          <el-table-column prop="fee" :label="$t('listHeader.fee')">
+            <template slot-scope="scope">{{scope.row.fee}}
+              <span class="unit">Fan</span>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="page">
+          <el-pagination class="el-pagination-wrap fr" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="page" :page-size="pageSize" layout="prev, pager, next" :total="total">
+          </el-pagination>
+        </div>
+      </div>
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
 <script>
 import { mapActions } from 'vuex'
@@ -113,7 +81,6 @@ export default {
       title: this.$t('navs.block'),
       content: 'A total of more than > 10,000,000 blocks found (showing the last 100000 records only)',
       link: this.$t('navs.block'),
-
       pageSize: 25
     }
   },
