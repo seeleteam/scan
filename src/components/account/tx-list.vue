ccb36f8b838c2f80b5c2210356fef0662b4a3e3c
MiyaFlower <yangmingyan1987@163.com>
2018-09-28 16:12:09 +0800

    modify account

--- src/components/account/tx-list.vue
+++ src/components/account/tx-list.vue
@@ -1,107 +1,80 @@
 <template>
-    <div class="page-wrap">
-        <div class="wrap lg-show">
-          <Header></Header>
-        </div>
-        <div class="sm-show page-sm-header-wrap">
-          <smHeader></smHeader>
-        </div>
-        <div class="sm-show sm-search-input-wrap">
-          <searchInput></searchInput>
-        </div>
-        <div class="main-wrap">
-          <div class="wrap">
-            <div class="describe-title-wrap">
-              <span class="title">{{$t("navs.accountTx")}}</span>
-              <ul class="link-wrap">
-                <li><a href="/">{{$t("navs.home")}}</a></li>
-                <li><i class="el-icon-arrow-right"></i></li>
-                <li class="current">{{$t("navs.accountTx")}}</li>
-              </ul>
-            </div>
-            <div class="des-title">
-              {{$t("navs.accountAddress")}}: {{title}}
-            </div>
-            <el-table
-              class="list-wrap"
-              :empty-text="$t('message.noData')"
-              :data="accountTxList"
-              style="width: 100%; background: transparent">
-              <el-table-column
-                prop="hash"
-                width="230"
-                :label="$t('listHeader.hash')">
-                <template slot-scope="scope">
-                  <router-link :to="{path: '/transaction/detail', query: { txhash: scope.row.hash }}">
-                    <span class="list-content table-link-color">{{scope.row.hash}}</span>
-                  </router-link>
-                </template>
-              </el-table-column>
-              <!-- <el-table-column
-                prop="age"
-                width="120"
-                :label="$t('listHeader.age')">
-              </el-table-column> -->
-              <!-- <el-table-column
-                prop="amount"
-                width="100"
-                :label="$t('listHeader.amount')">
-              </el-table-column> -->
-              <el-table-column
-                prop="block"
-                :label="$t('listHeader.block')"
-                width="120">
-              </el-table-column>
-              <el-table-column
-                prop="from"
-                :label="$t('listHeader.from')"
-                width="240">
-                <template slot-scope="scope">
-                  <span v-if="scope.row.inorout === true" :class="{'table-link-color': isLink(scope.row.from)}" class="list-content" @click="toTx(scope.row.from)">{{[scope.row.from, 'from'] | setFormatAd}}</span>
-                  <span v-else class="list-content">{{[scope.row.from, 'from'] | setFormatAd}}</span>
-                </template>
-              </el-table-column>
-              <el-table-column
-                prop="inorout"
-                width="100"
-                :label="$t('listHeader.inorout')">
-                <template slot-scope="scope">
-                  <span v-if="scope.row.inorout === true" class="list-content list-content-in">{{$t('tx.in')}}</span>
-                  <span v-else class="list-content list-content-out">{{$t('tx.out')}}</span>
-                </template>
-              </el-table-column>
-              <el-table-column
-                prop="to"
-                width="240"
-                :label="$t('listHeader.to')">
-                <template slot-scope="scope">
-                  <span v-if="scope.row.inorout === false" :class="{'table-link-color': isLink(scope.row.to)}" class="list-content" @click="toTx(scope.row.to)">{{[scope.row.to, 'to'] | setFormatAd}}</span>
-                  <span v-else class="list-content">{{[scope.row.to, 'to'] | setFormatAd}}</span>
-                </template>
-              </el-table-column>
-              <el-table-column
-                prop="value"
-                :label="$t('listHeader.value')">
-              </el-table-column>
-              <el-table-column
-                prop="fee"
-                :label="$t('listHeader.txfee')">
-                <template slot-scope="scope">{{scope.row.fee}} <span class="unit">Fan</span></template>
-              </el-table-column>
-            </el-table>
-            <el-pagination
-              class="el-pagination-wrap fr"
-              @size-change="handleSizeChange"
-              @current-change="handleCurrentChange"
-              :current-page="page"
-              :page-size="pageSize"
-              layout="prev, pager, next"
-              :total="total">
-            </el-pagination>
-          </div>
-        </div>
-        <Footer></Footer>
+  <div class="page-wrap">
+    <!-- PC header -->
+    <Header></Header>
+    <!-- mobile header -->
+    <div class="sm-show page-sm-header-wrap">
+      <smHeader></smHeader>
     </div>
+    <!-- search input -->
+    <div class="sm-show sm-search-input-wrap">
+      <searchInput></searchInput>
+    </div>
+    <div class="main-wrap">
+      <div class="wrap">
+        <!-- account detail page breadNav -->
+        <div class="describe-title-wrap">
+          <!-- breadNav animate effect -->
+          <vue-particles color="#fff" :particleOpacity="0.7" :particlesNumber="80" shapeType="circle" :particleSize="4" linesColor="#000" :linesWidth="1" :lineLinked="true" :lineOpacity="0.4" :linesDistance="150" :moveSpeed="3" :hoverEffect="true" hoverMode="grab" :clickEffect="true" clickMode="push" class="particles"></vue-particles>
+          <span class="title">{{$t("navs.accountTx")}}</span>
+          <ul class="link-wrap">
+            <li>
+              <a href="/">{{$t("navs.home")}}</a>
+            </li>
+            <li>
+              <i class="el-icon-arrow-right"></i>
+            </li>
+            <li class="current">{{$t("navs.accountTx")}}</li>
+          </ul>
+        </div>
+        <div class="des-title">
+          {{$t("navs.accountAddress")}}: {{title}}
+        </div>
+        <!-- account list -->
+        <el-table class="list-wrap" :empty-text="$t('message.noData')" :data="accountTxList" style="width: 100%; background: transparent">
+          <el-table-column prop="hash" width="230" :label="$t('listHeader.hash')">
+            <template slot-scope="scope">
+              <router-link :to="{path: '/transaction/detail', query: { txhash: scope.row.hash }}">
+                <span class="list-content table-link-color">{{scope.row.hash}}</span>
+              </router-link>
+            </template>
+          </el-table-column>
+          <el-table-column prop="block" :label="$t('listHeader.block')" width="120">
+          </el-table-column>
+          <el-table-column prop="from" :label="$t('listHeader.from')" width="240">
+            <template slot-scope="scope">
+              <span v-if="scope.row.inorout === true" :class="{'table-link-color': isLink(scope.row.from)}" class="list-content" @click="toTx(scope.row.from)">{{[scope.row.from, 'from'] | setFormatAd}}</span>
+              <span v-else class="list-content">{{[scope.row.from, 'from'] | setFormatAd}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="inorout" width="100" :label="$t('listHeader.inorout')">
+            <template slot-scope="scope">
+              <span v-if="scope.row.inorout === true" class="list-content list-content-in">{{$t('tx.in')}}</span>
+              <span v-else class="list-content list-content-out">{{$t('tx.out')}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="to" width="240" :label="$t('listHeader.to')">
+            <template slot-scope="scope">
+              <span v-if="scope.row.inorout === false" :class="{'table-link-color': isLink(scope.row.to)}" class="list-content" @click="toTx(scope.row.to)">{{[scope.row.to, 'to'] | setFormatAd}}</span>
+              <span v-else class="list-content">{{[scope.row.to, 'to'] | setFormatAd}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="value" :label="$t('listHeader.value')">
+          </el-table-column>
+          <el-table-column prop="fee" :label="$t('listHeader.txfee')">
+            <template slot-scope="scope">{{scope.row.fee}}
+              <span class="unit">Fan</span>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="page">
+          <el-pagination class="el-pagination-wrap fr" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="page" :page-size="pageSize" layout="prev, pager, next" :total="total">
+          </el-pagination>
+        </div>
+      </div>
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
 <script>
 import { mapActions } from 'vuex'
@@ -126,25 +99,30 @@ export default {
     Footer
   },
   filters: {
+    // judge data is it empty
     setFormatAd (params) {
       return formatAd(params)
     }
   },
   mounted () {
+    // parameter assignment
     this.getList(1, this.$route.query)
     this.title = this.$route.query.address
   },
   computed: {
+    // get account data
     accountTxList: {
       get () {
         return this.$store.state.account.accountTxList
       }
     },
+    // get list page
     page: {
       get () {
         return this.$store.state.account.page
       }
     },
+    // get list total number
     total: {
       get () {
         return this.$store.state.account.total
@@ -153,19 +131,23 @@ export default {
   },
   methods: {
     ...mapActions(['getAccountTxList']),
-
+    // get parameter
     handleSizeChange (val) {
       this.getList(val, this.$route.query)
     },
+    // get parameter
     handleCurrentChange (val) {
       this.getList(val, this.$route.query)
     },
+    // get parameter
     getList (page, address) {
       this.getAccountTxList([page, address])
     },
+    // jump to this.hash address
     toTx (txHash) {
       return filtersAd(txHash) ? router.push({path: '/account/detail', query: { address: txHash }}) : ''
     },
+    // filter address
     isLink (address) {
       return filtersAd(address)
     }
