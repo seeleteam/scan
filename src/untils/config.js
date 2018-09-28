cbfe19b3c5b05d4bdbb7d46e48578edee47e4202
MiyaFlower <yangmingyan1987@163.com>
2018-09-28 11:29:41 +0800

    config

--- src/untils/config.js
+++ src/untils/config.js
@@ -1,20 +1,27 @@
 // localhost:3003
-const URL = (process.env.SCAN_API_URL || 'http://localhost:3003') + (process.env.SCAN_API_PATH || '/api/v1')
+const URL = (process.env.SCAN_API_URL || 'https://localhost:3003') + (process.env.SCAN_API_PATH || '/api/v1')
 module.exports = {
   name: 'seele-scan',
-  CORS: [process.env.SCAN_API_URL || 'http://localhost:3003'],
+  CORS: [process.env.SCAN_API_URL || 'https://localhost:3003'],
   YQL: [''],
   API: {
-    txcountUrl: `${URL}/txcount`,
-    lastblockUrl: `${URL}/lastblock`,
-    bestblockUrl: `${URL}/bestblock`,
-    avgblocktimeUrl: `${URL}/avgblocktime`,
-    hashrateUrl: `${URL}/hashrate`,
-    networkdifficultyUrl: `${URL}/difficulty`,
-    blocksCountUrl: `${URL}/blockcount`,
-    transactionsCountUrl: `${URL}/txcount`,
-    accountsCountUrl: `${URL}/accountcount`,
-    contractsCountUrl: `${URL}/contractcount`,
+
+    // last block time
+    lastBlockTimeUrl: `${URL}/blockprotime`,
+    // transaction total
+    transactionTotalUrl: `${URL}/txcount?test=1`,
+    // index tps
+    blockTxsTpsUrl: `${URL}/blockTxsTps`,
+    // account total
+    accountTotalUrl: `${URL}/accountcount?test=1`,
+    // contract total
+    contractTotalUrl: `${URL}/contractcount?test=1`,
+    // chart data
+    chartDataUrl: `${URL}/Txstat`,
+    // account balance ranking
+    accountRankingUrl: `${URL}/Homeaccounts`,
+    // miner ranking
+    minerRankingUrl: `${URL}/miners?test=1`,
 
     blocksUrl: `${URL}/blocks`,
     blocksDetailUrl: `${URL}/block`,
