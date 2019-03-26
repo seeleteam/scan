6a9f3c92ff1d1539fdd5bffd60444f138b358d72
avadesian <chunxiangxu@gmail.com>
2019-03-26 18:44:43 +0800

    change shard number from 20 to 4

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   NEW_WORK_ID: 2,
-  SCAN_SHARD: 20,
+  SCAN_SHARD: 4,
   SCAN_API_URL: '"https://api.seelescan.net"',   // http://106.75.86.27:8888
   SCAN_API_PATH: '"/api/v1"'
 })
--- config/test.env.js
+++ config/test.env.js
@@ -5,7 +5,7 @@ const devEnv = require('./dev.env')
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
   NEW_WORK_ID: 2,
-  SCAN_SHARD: 20,
+  SCAN_SHARD: 4,
   SCAN_API_URL: '"https://api.seelescan.net"',   // http://106.75.86.27:8888
   SCAN_API_PATH: '"/api/v1"'
 })
